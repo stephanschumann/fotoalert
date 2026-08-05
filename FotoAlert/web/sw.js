b589e0bfa4c32275e71f9e60669960b9b04bee96
Stephan Schumann <stephanschumann@me.com>
2026-08-05 22:33:21 +0200

    release: v1.22.58 – BUG-89: Inline-Hinweis Wetter-Score vs. Wolkenstimmung ergaenzt

--- FotoAlert/web/sw.js
+++ FotoAlert/web/sw.js
@@ -1,5 +1,5 @@
 // FotoAlert Service Worker – Offline-Cache + Push Notifications
-const CACHE_NAME = 'fotoalert-v1.22.57';
+const CACHE_NAME = 'fotoalert-v1.22.58';
 const STATIC_ASSETS = ['/', '/manifest.json'];
 
 self.addEventListener('install', e => {
