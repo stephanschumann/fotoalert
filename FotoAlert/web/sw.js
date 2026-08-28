a5388d47cff0e59d0d51788eacb9658514c7f1af
Stephan Schumann <stephanschumann@me.com>
2026-08-28 20:59:16 +0200

    release: v1.22.69 – BUG-111: Wetter-API-Drosselung neu kalibriert

--- FotoAlert/web/sw.js
+++ FotoAlert/web/sw.js
@@ -1,5 +1,5 @@
 // FotoAlert Service Worker – Offline-Cache + Push Notifications
-const CACHE_NAME = 'fotoalert-v1.22.68';
+const CACHE_NAME = 'fotoalert-v1.22.69';
 const STATIC_ASSETS = ['/', '/manifest.json'];
 
 self.addEventListener('install', e => {
